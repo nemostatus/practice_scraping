36b7d2a64e9fd1a7009db369a646d216031c5537
Matthew <mattcorralejo1@gmail.com>
2020-12-24 03:00:37 -0800

    using byebug to see what the parsed data looks like

--- scraper.rb
+++ scraper.rb
@@ -2,7 +2,15 @@ require 'httparty'
 require 'nokogiri'
 require 'byebug'
 require 'json'
+require 'pry'
 
 def scrape_books 
-    unparsed_data = HTTParty.get('books.toscrape.com')
+ 
+    unparsed_data = HTTParty.get('https://books.toscrape.com/')
+
+    parsed_page = Nokogiri::HTML(unparsed_data)
+  
+   puts "hi"
+  byebug
 end
+scrape_books
