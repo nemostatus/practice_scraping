d62d84ede4b54af1849d66980b0671e438dcb8b8
Matthew <mattcorralejo1@gmail.com>
2020-12-24 03:37:56 -0800

    trying to figure it out

--- scraper.rb
+++ scraper.rb
@@ -5,12 +5,10 @@ require 'json'
 require 'pry'
 
 def scrape_books 
- 
-    unparsed_data = HTTParty.get('https://books.toscrape.com/')
-
-    parsed_page = Nokogiri::HTML(unparsed_data)
-  
-   puts "hi"
+ unparsed_data = HTTParty.get('https://books.toscrape.com/')
+ parsed_page = Nokogiri::HTML(unparsed_data)
+  #i want to get titles and ratings
+#   title = parsed_page.css("a").first["title"]
   byebug
 end
 scrape_books
