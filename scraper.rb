12be5d49ff111542dd4b032254aec3c24468e263
Matthew <mattcorralejo1@gmail.com>
2021-01-01 06:22:06 -0800

    I created an empty array to push all of the titles into and made a puts statement after iteration which displays names in terminal

--- scraper.rb
+++ scraper.rb
@@ -10,7 +10,12 @@ def scrape_books
   #i want to get titles and ratings
 #   title = parsed_page.css("a").first["title"]
 #create variable and test what that dat is w/byebug by running ruby and file
-testing = parsed_page.css('ol').css('.row').css('h3').text
- byebug
+testing = parsed_page.css('ol').css('.row').css('h3')
+book_titles = []
+testing.each do |title|
+attempt = title.text
+book_titles << attempt
+end
+puts book_titles
 end
 scrape_books
