b8049e3a88b943bcbbaedee00334c3a115c6d60c
Matthew <mattcorralejo1@gmail.com>
2021-01-01 08:34:15 -0800

    i need to figure out how to get class of star rating name so i can get ratings

--- scraper.rb
+++ scraper.rb
@@ -12,14 +12,20 @@ def scrape_books_titles_and_ratings
 #   title = parsed_page.css("a").first["title"]
 #create variable and test what that dat is w/byebug by running ruby and file
 testing = parsed_page.css('ol').css('.row').css('h3')
-ratings = parsed_page.css('ol').css('.row').css('p').css('.star-rating')
-book_titles_and_ratings = []
+ratings = parsed_page.css('ol').css('.row').css('p').css('star-rating')
+book_titles=[]
+book_ratings =[]
+#ratings
+ratings.each do |rating|
+try = ratings.text
+book_ratings << try
+end
 testing.each do |title|
 attempt = title.text
-book_titles_and_ratings << attempt
+book_titles << attempt
 end
 #now get ratings 
-puts ratings.css
+byebug
 end
 scrape_books_titles_and_ratings #invoking method
 
